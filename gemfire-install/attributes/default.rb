ef19dee33545bc23a37c9681907359b82dda3633
jitheshktv <jiteshkumar.thekkeveettil@citi.com>
2016-04-18 16:52:12 -0500

    changes the java_home path in attributes and removed from directories recipe

--- gemfire-install/attributes/default.rb
+++ gemfire-install/attributes/default.rb
@@ -13,6 +13,6 @@ default['gemfire-install']['install']['bin_target_tmp'] = '/tmp'
 
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
-default['java']['java_home'] = '/opt/jre'
+default['java']['java_home'] = '/usr/lib/jvm/java'
 default['java']['set_etc_environment'] = 'true'
 default['java']['oracle']['accept_oracle_download_terms'] = 'true'
--- gemfire-install/recipes/directories.rb
+++ gemfire-install/recipes/directories.rb
@@ -10,12 +10,11 @@
 gemfire_install_dir = node['gemfire-install']['install']['gemfire_install_dir']
 gemfire_config_dir = node['gemfire-install']['install']['gemfire_config_dir']
 logs_home_dir = node['gemfire-install']['install']['logs_home_dir']
-java_home = node['java']['java_home']
 
 user_name = node['gemfire-install']['install']['user']
 group_name = node['gemfire-install']['install']['group']
 
-[gemfire_install_dir, logs_home_dir, gemfire_config_dir, java_home].each do |dir_name|
+[gemfire_install_dir, logs_home_dir, gemfire_config_dir].each do |dir_name|
   directory dir_name do
     owner user_name
     group group_name
